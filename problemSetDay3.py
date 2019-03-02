f8291ccbc1f12f42f3d0d46f7c1417f5a5190a44
anagarcia02 <annertimes@gmail.com>
2019-03-02 15:37:08 +0000

    rock paper scissors 3/2

--- problemSetDay3.py
+++ problemSetDay3.py
@@ -1,1 +1,42 @@
 # File name: problemSetDay3.py
+from random import randint
+
+player = input('Rock (r), paper (p), or scissors (s)?')
+
+print(player, 'vs')
+
+chosen = randint(1, 3)
+
+# print(chosen)
+
+if chosen == 1:
+    computer = 'r'
+
+elif chosen == 2:
+    computer = 'p'
+
+else:
+    computer = 's'
+
+print(computer)
+
+if player == computer:
+    print('Draw!')
+
+elif player == 'r' and computer == 's':
+    print('Player wins!')
+
+elif player == 'r' and computer == 'p':
+    print('Computer wins!')
+
+elif player == 'p' and computer == 'r':
+    print('Player wins!')
+
+elif player == 'p' and computer == 's':
+    print('Computer wins!')
+
+elif player == 's' and computer == 'p':
+    print('Player wins!')
+
+elif player == 's' and computer == 'r':
+    print('Computer wins!')
---
+++ warmupDay4.py
@@ -0,0 +1,16 @@
+month = input('Pick any month of the year.')
+
+if month == january:
+    print('There are 31 days in January.')
+
+if month == february:
+    print('There are 31 days in February.')
+
+if month == march:
+    print('There are 31 days in January.')
+
+if month == april:
+    print('There are 31 days in January.')
+
+if month == january
+    print('There are 31 days in January.')
